f26496991acc5eb65e6cc433717b108ad74e3ada
Sushil97 <sushil500530@gmail.com>
2023-12-22 16:12:51 +0600

    add verify token and clear cookie

--- index.js
+++ index.js
@@ -2,12 +2,23 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 const { MongoClient, ServerApiVersion } = require('mongodb');
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser')
 const app = express();
 const port = process.env.PORT || 5000;
 
 // middleware 
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+    "http://localhost:5174",
+  ],
+  credentials: true,
+  optionsSuccessStatus: 200,
+}));
+app.use(express.json());
+app.use(cookieParser());
 
-  
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ruakr2a.mongodb.net/?retryWrites=true&w=majority`;
   
   // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -45,6 +56,39 @@ async function run() {
       }
     })
 
+      // verify token 
+      const verifyToken = async (req, res, next) => {
+        try {
+          const token = req.cookies?.token;
+          // console.log('token is found?-------->', token);
+          if (!token) {
+            return res.status(401).send({ message: 'not authorized access' })
+          }
+          jwt.verify(token, process.env.ACCESS_SECRET_TOKEN, (error, decode) => {
+            if (error) {
+              return res.status(401).send({ message: 'unAuthorized access why' })
+            }
+            req.user = decode;
+            next();
+          })
+        }
+        catch (error) {
+          res.status(401).send({ success: false, message: error.message })
+        }
+      }
+  
+      // post method start 
+      app.post('/logout', async (req, res) => {
+        try {
+          const user = req.body;
+          // console.log('log out user is in---->', user);
+          res.clearCookie('token', { maxAge: 0, sameSite: 'none', secure: true }).send({ success: true })
+        }
+        catch (error) {
+          console.log(error);
+        }
+      })
+  
 
     // Send a ping to confirm a successful connection
     // await client.db("admin").command({ ping: 1 });
